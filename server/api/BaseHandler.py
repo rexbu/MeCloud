91b5ebe6d0904bbc34e7be28c9db8a1cc253fa7f
rexbu <rex@labjk.com>
2019-02-12 20:19:30 +0800

    auth

--- server/api/BaseHandler.py
+++ server/api/BaseHandler.py
@@ -33,6 +33,7 @@ class BaseHandler(tornado.web.RequestHandler):
         userid = self.get_secure_cookie("u")
         if userid is None:
             userid = self.get_userid_from_cookie(str(self.get_cookie("u")))
+        self.userid = userid
         return userid
 
     def prepare(self):
@@ -174,6 +175,8 @@ class BaseHandler(tornado.web.RequestHandler):
         return
 
     def check_field(self, classname, obj):  # 检查字段存在与否，字段范围，字段默认值，字段关联，记录权限等
+        ###TODO: 暂时不检查
+        return True
         try:
             checkInfo = BaseConfig.projectClass[classname]
         except Exception, ex:
